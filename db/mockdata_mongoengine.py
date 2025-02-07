b95d3a8e03e04472de732ab5529bf9ea53cdf172
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-07 20:59:29 +0200

    perform validation in a loop

--- db/mockdata_mongoengine.py
+++ db/mockdata_mongoengine.py
@@ -1,15 +1,18 @@
 from models_mongoengine import Users, Content, FollowerList, PublicList, PrivateList
 
 users = [
-    Users(username="John Doe").validate(),
-    Users(username="Foo Bar").validate(),
-    Users(username="Elon Musk").validate()
+    Users(username="John Doe"),
+    Users(username="Foo Bar"),
+    Users(username="Elon Musk")
 ]
+for user in users:
+    user.validate()
 Users.objects.insert(users)
 
 contents = [
-    Content(name="Inception", content_type="movie").validate(),
-    Content(name="Deadpool", content_type="movie").validate()
+    Content(name="Inception", content_type="movie"),
+    Content(name="Deadpool", content_type="movie")
 ]
-
+for content in contents:
+    content.validate()
 Content.objects.insert(contents)
