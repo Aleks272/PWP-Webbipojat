8ebd8b1bda0ab269a308b5b7236e78119f1786b7
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-07 20:54:16 +0200

    insert() skips validation, need to add it explicitly

--- db/mockdata_mongoengine.py
+++ db/mockdata_mongoengine.py
@@ -1,15 +1,15 @@
 from models_mongoengine import Users, Content, FollowerList, PublicList, PrivateList
 
 users = [
-    Users(username="John Doe"),
-    Users(username="Foo Bar"),
-    Users(username="Elon Musk")
+    Users(username="John Doe").validate(),
+    Users(username="Foo Bar").validate(),
+    Users(username="Elon Musk").validate()
 ]
 Users.objects.insert(users)
 
 contents = [
-    Content(name="Inception", content_type="movie"),
-    Content(name="Deadpool", content_type="movie")
+    Content(name="Inception", content_type="movie").validate(),
+    Content(name="Deadpool", content_type="movie").validate()
 ]
 
 Content.objects.insert(contents)
