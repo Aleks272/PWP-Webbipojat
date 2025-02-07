81177ea34e9b8cdf612c8f013a4089d826be6de9
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-07 19:37:10 +0200

    Add rest of the models, add meta dicts for unique combinations

--- db/models_mongoengine.py
+++ db/models_mongoengine.py
@@ -1,15 +1,44 @@
-from mongoengine import Document, StringField, SequenceField, EnumField, connect
+from mongoengine import Document, StringField, SequenceField, EnumField, IntField, connect
 from enum import Enum
 
 class Users(Document):
-    person_id: SequenceField()
-    username: StringField(unique=True)
+    person_id = SequenceField()
+    username = StringField(unique=True)
 
 class ContentType(Enum):
     movie = "movie"
     series = "series"
 
 class Content(Document):
-    content_id: SequenceField()
-    name: StringField(unique=True)
-    content_type: EnumField(ContentType, default=ContentType.movie)
+    content_id = SequenceField()
+    name = StringField(unique=True)
+    content_type = EnumField(ContentType, default=ContentType.movie)
+
+class FollowerList(Document):
+    person_id = IntField()
+    following_id = IntField()
+    meta = {
+        'indexes': [
+            {'fields': ('person_id', 'following_id'), 'unique': True}
+        ]
+    }
+
+class PublicList(Document):
+    user_note = StringField()
+    person_id = IntField()
+    content_id = IntField()
+    meta = {
+        'indexes': [
+            {'fields': ('person_id', 'content_id'), 'unique': True}
+        ]
+    }
+
+class PrivateList(Document):
+    user_note = StringField()
+    person_id = IntField()
+    content_id = IntField()
+    meta = {
+        'indexes': [
+            {'fields': ('person_id', 'content_id'), 'unique': True}
+        ]
+    }
