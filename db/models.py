65f97f08eb2e9c114ec05b4fc4731030a85fa822
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-07 08:55:22 +0200

    Make username unique in Users-model

--- db/models.py
+++ db/models.py
@@ -1,5 +1,6 @@
 from beanie import Document, Indexed
+from pydantic import Field
 
 class Users(Document):
     person_id: Indexed(int, unique=True)
-    username: str
+    username: str = Field(unique=True)
