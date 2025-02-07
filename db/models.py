01de805e33cbaec7047a638a78d63ea48d18da24
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-07 09:56:11 +0200

    Add python-dotenv, initialize DB connection from a MongoDB string in .env

--- db/models.py
+++ db/models.py
@@ -1,6 +1,13 @@
-from beanie import Document, Indexed
+import os
+from bunnet import Document, Indexed, init_bunnet
 from pydantic import Field
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
 
 class Users(Document):
     person_id: Indexed(int, unique=True)
     username: str = Field(unique=True)
+
+client = MongoClient(os.getenv("MONGODB_CONNECTION_STRING"))
+init_bunnet(database=client.db_name, document_models=[Users])
