3aa63e9c0d2dadce264e43c7bda6eaed57ddafc2
tominikula <tomi.nikula01@gmail.com>
2025-02-26 15:51:29 +0200

    user.py test example

--- project_watchlist/resources/user.py
+++ project_watchlist/resources/user.py
@@ -3,6 +3,19 @@ from flask_restful import Resource
 from jsonschema import validate, ValidationError
 from werkzeug.exceptions import NotFound, Conflict, BadRequest, UnsupportedMediaType
 from werkzeug.routing import BaseConverter
+from models import Users
+import mongoengine
+
+class UserConverter(BaseConverter):
+
+    def to_python(self, value):
+        db_user = Users.objects(person_id=value).first()
+        if db_user is None:
+            raise NotFound
+        return db_user
+
+    def to_url(self, value):
+        return str(value.person_id)
 
 class UserItem(Resource):
     def get(self):
@@ -14,6 +27,18 @@ class UserItem(Resource):
     def delete(self):
         pass
 
+    @staticmethod
+    def json_schema():
+        schema = {
+            "type": "object",
+            "required": ["username"]
+        }
+        properties = schema["properties"] = {}
+        properties["username"] = {
+            "description": "Username",
+            "type": "string"
+        }
+        return schema
 
 class UserCollection(Resource):
     def get(self):
@@ -22,3 +47,25 @@ class UserCollection(Resource):
     def post(self, user):
         if not request.json:
             abort(415, "unsupported media type")
+        try:
+            validate(request.json, UserItem.json_schema())
+            new_User = Users(
+                username=request.json["username"]
+            )
+            Users.objects.insert(new_User)
+            return Response(
+                "New user added", 
+                status=201, 
+                mimetype="application/json",
+                headers={"Location": url_for(
+                    "api.useritem", 
+                    person_id=new_User.person_id
+                    )
+                }
+            )
+        except ValidationError as e:
+            abort(400, str(e))
+        except KeyError:
+            abort(400, "Incomplete request - missing fields")
+        except mongoengine.ValidationError:
+            abort(400, "Database validation error")
--- project_watchlist/watchlist_api.py
+++ project_watchlist/watchlist_api.py
@@ -3,13 +3,14 @@ from flask_restful import Api
 from mongoengine import connect
 import os
 from dotenv import load_dotenv
-from resources.user import UserItem
 
 load_dotenv()
 connect(host=os.getenv("MONGODB_CONNECTION_STRING"), name="db")
 
 app = Flask(__name__)
 api = Api(app)
+
+from resources.user import UserItem, UserConverter
 
 app.url_map.converters["user"] = UserConverter
 api.add_resource(UserItem, "/api/users/<user:user>/")
