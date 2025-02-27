8f56599a825c169687fb9baa2488a17c9e40b428
Aleksanteri <aleks.anteri27@gmail.com>
2025-02-27 12:15:29 +0100

    Mockdata module import fixed

--- tests/mockdata.py
+++ tests/mockdata.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from project_watchlist.models import Users, Content, FollowerList, Watchlist, ContentType
 
 users = [
