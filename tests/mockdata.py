80aa0587cc04cb640bee4228abd35645bc55974a
Aleksanteri <aleks.anteri27@gmail.com>
2025-02-27 11:55:15 +0100

    Mockdata.py now uses watchlist model instead of old public/private model

--- tests/mockdata.py
+++ tests/mockdata.py
@@ -1,4 +1,4 @@
-from project_watchlist.models import Users, Content, FollowerList, PublicList, PrivateList, ContentType
+from project_watchlist.models import Users, Content, FollowerList, Watchlist, ContentType
 
 users = [
     Users(username="John Doe"),
@@ -38,26 +38,27 @@ following = Users.objects(username="Foo Bar").first().person_id
 new_follow = FollowerList(person_id=person, following_id=following)
 new_follow.save()
 
-#PublicList
+
+#WatchlistPublic1
 person = Users.objects(username="Elon Musk").first().person_id
 content = Content.objects(name="Breaking Bad").first().content_id
-new_entry = PublicList(person_id=person, content_id=content, user_note="ok")
+new_entry = Watchlist(person_id=person, content_id=content, user_note="ok", public_entry=True)
 new_entry.save()
 
-#PublicList2
+#WatchlistPublic2
 person = Users.objects(username="Elon Musk").first().person_id
 content = Content.objects(name="Inception").first().content_id
-new_entry = PublicList(person_id=person, content_id=content, user_note="Not ok")
+new_entry = Watchlist(person_id=person, content_id=content, user_note="Not ok", public_entry=True)
 new_entry.save()
 
-#PrivateList
+#WatchlistPrivate1
 person = Users.objects(username="Elon Musk").first().person_id
 content = Content.objects(name="Deadpool").first().content_id
-new_entry = PrivateList(person_id=person, content_id=content, user_note="Not ok")
+new_entry = Watchlist(person_id=person, content_id=content, user_note="Not ok", public_entry=False)
 new_entry.save()
 
-#PrivateList2
+#WatchlistPrivate2
 person = Users.objects(username="Elon Musk").first().person_id
 content = Content.objects(name="Inception").first().content_id
-new_entry = PrivateList(person_id=person, content_id=content, user_note="Tesla Model SS")
+new_entry = Watchlist(person_id=person, content_id=content, user_note="Tesla Model SS", public_entry=False)
 new_entry.save()
