b8db39e57dff10ba427d0b5ec5b00182b03c95ad
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-03-01 15:23:10 +0200

    Add tests for ContentItem

--- tests/api_test.py
+++ tests/api_test.py
@@ -335,3 +335,81 @@ class TestWatchListItem(object):
         """
         res = client.delete(self.WRONG_RESOURCE_URL)
         assert res.status_code == 404
+
+class TestContentItem():
+    """
+    Tests for content item resource
+    """
+
+    RESOURCE_URL = "/api/content/1/"
+    WRONG_RESOURCE_URL = "/api/content/100/"
+
+    def test_get_existing_content(self, client):
+        """
+        Test that we can get an existing content
+        """
+        res = client.get(self.RESOURCE_URL)
+        assert res.status_code == 200
+        response_body = json.loads(res.data)
+        assert response_body["content_id"] == 1
+        assert response_body["name"] == "Inception"
+        assert response_body["content_type"] == "MOVIE"
+    
+    def test_get_nonexistent_content(self, client):
+        """
+        Test that we cannot get nonexistent content
+        """
+        res = client.get(self.WRONG_RESOURCE_URL)
+        assert res.status_code == 404
+    
+    def test_put_with_valid_data(self, client):
+        """
+        Test that we can update content with valid data
+        """
+        data = {
+            "name": "Titanic",
+            "content_type": "MOVIE"
+        }
+        res = client.put(self.RESOURCE_URL, json=data)
+        assert res.status_code == 200
+        # check that the content changed
+        res = client.get(self.RESOURCE_URL)
+        response_body = json.loads(res.data)
+        assert response_body["name"] == data["name"]
+        assert response_body["content_type"] == data["content_type"]
+
+    def test_put_with_missing_fields(self, client):
+        """
+        Test that we cannot update content with missing fields
+        """
+        data = {
+            "name": "Titanic"
+        }
+        res = client.put(self.RESOURCE_URL, json=data)
+        assert res.status_code == 400
+
+    def test_put_with_unsupported_media_type(self, client):
+        """
+        Test that we get proper error for unsupported media type
+        """
+        res = client.put(self.RESOURCE_URL,
+                         data="test",
+                         headers=Headers({"Content-Type": "text"}))
+        assert res.status_code == 415
+
+    def test_delete_existing_content(self, client):
+        """
+        Test that we can delete existing content
+        """
+        res = client.delete(self.RESOURCE_URL)
+        assert res.status_code == 200
+        # check that the resource is deleted
+        res = client.get(self.RESOURCE_URL)
+        assert res.status_code == 404
+    
+    def test_delete_nonexistent_content(self, client):
+        """
+        Test that we get error when deleting nonexistent content
+        """
+        res = client.delete(self.WRONG_RESOURCE_URL)
+        assert res.status_code == 404
