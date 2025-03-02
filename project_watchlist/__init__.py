1862d762e3772c3a813bfa9e252e530b6d95b884
Emil Kelhälä <emil.kelhala@gmail.com>
2025-03-02 09:06:15 +0200

    Added jwt initializations to __init__

--- project_watchlist/__init__.py
+++ project_watchlist/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from mongoengine import connect
 import os
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
 
 load_dotenv()
 
@@ -13,11 +14,17 @@ def create_app(test_mode=False):
     connect(host=os.getenv("MONGODB_CONNECTION_STRING"),
             name=db_name,
             uuidRepresentation="standard")
+    
+    # TODO: change this to something from .env
+    app.config["JWT_SECRET_KEY"] = "secret"
+    jwt_manager = JWTManager(app)
 
     from project_watchlist import watchlist_api
+    from project_watchlist import auth
     from project_watchlist.utils import UserConverter, WatchlistConverter, ContentConverter
     app.url_map.converters["user"] = UserConverter
     app.url_map.converters["watchlist"] = WatchlistConverter
     app.url_map.converters["content"] = ContentConverter
     app.register_blueprint(watchlist_api.api_bp)
+    app.register_blueprint(auth.auth_bp)
     return app
