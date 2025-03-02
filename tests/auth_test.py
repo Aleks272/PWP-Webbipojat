040a52717ab738594f5cedd3976e27f935ec192f
Emil Kelhälä <emil.kelhala@gmail.com>
2025-03-02 12:30:39 +0200

    Add login tests

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -18,3 +18,54 @@ def client():
     db = mongoengine.get_connection()
     db.drop_database('test_db')
     mongoengine.disconnect_all()
+
+class TestAuthLogin():
+
+    RESOURCE_URL = "/auth/login/"
+
+    def test_login_with_valid_credentials(self, client):
+        """
+        Test that we can login with valid credentials and get an access token
+        """
+        data = {
+            "username": "johndoe",
+            "password": "password"
+        }
+        res = client.post(self.RESOURCE_URL, json=data)
+        assert res.status_code == 200
+        # check that we get the token
+        response_body = json.loads(res.data)
+        assert response_body["token"]
+
+    def test_login_with_invalid_credentials(self, client):
+        """
+        Test that we cannot login with invalid credentials
+        """
+        data = {
+            "username": "johndoe",
+            "password": "wrongpassword"
+        }
+        res = client.post(self.RESOURCE_URL,
+                          json=data)
+        assert res.status_code == 401
+
+    def test_login_with_unsupported_media_type(self, client):
+        """
+        Test that login fails with unsupported media type
+        """
+        res = client.post(self.RESOURCE_URL,
+                          data="username=johndoe;password=password",
+                          headers=Headers({
+                            "Content-Type": "text"
+                          }))
+        assert res.status_code == 415
+
+    def test_login_with_missing_fields(self, client):
+        """
+        Test that login fails with missing fields
+        """
+        data = {
+            "username": "johndoe"
+        }
+        res = client.post(self.RESOURCE_URL, json=data)
+        assert res.status_code == 400
