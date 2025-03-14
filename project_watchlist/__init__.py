fb57075de050b1008e35bb8f185c46f496474c3f
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-03-14 10:35:55 +0200

    solve pylint warnings from __init__.py

--- project_watchlist/__init__.py
+++ project_watchlist/__init__.py
@@ -1,12 +1,18 @@
+"""
+Main module for watchlist API
+"""
+import os
 from flask import Flask
 from mongoengine import connect
-import os
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
 
 load_dotenv()
 
 def create_app(test_mode=False):
+    """
+    Creates and returns the app instance
+    """
     app = Flask(__name__)
     db_name = "db"
     if test_mode:
@@ -14,14 +20,17 @@ def create_app(test_mode=False):
     connect(host=os.getenv("MONGODB_CONNECTION_STRING"),
             name=db_name,
             uuidRepresentation="standard")
-    
     # TODO: change this to something from .env
     app.config["JWT_SECRET_KEY"] = "secret"
     jwt_manager = JWTManager(app)
 
     from project_watchlist import watchlist_api
     from project_watchlist import auth
-    from project_watchlist.utils import UserConverter, WatchlistConverter, ContentConverter, jwt_user_identity_loader, jwt_user_lookup_loader
+    from project_watchlist.utils import (UserConverter, 
+                                         WatchlistConverter,
+                                         ContentConverter, 
+                                         jwt_user_identity_loader, 
+                                         jwt_user_lookup_loader)
     # register loaders for JWT
     jwt_manager.user_identity_loader(jwt_user_identity_loader)
     jwt_manager.user_lookup_loader(jwt_user_lookup_loader)
